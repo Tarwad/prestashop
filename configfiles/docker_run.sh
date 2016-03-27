52771cc19997cac5890a18613f0ee5f80d59578e
Sebastien Libert <sebastien.libert@skynet.be>
2016-03-27 10:08:15 +0200

    some changes on docker_run.sh

--- configfiles/docker_run.sh
+++ configfiles/docker_run.sh
@@ -25,22 +25,22 @@ done
 if [ ! -f ./config/settings.inc.php  ]; then
 	if [ $PS_DEV_MODE -ne 0 ]; then
 		echo "\n* Enabling DEV mode ...";
-		sed -ie "s/define('_PS_MODE_DEV_', false);/define('_PS_MODE_DEV_',\ true);/g" /var/www/html/config/defines.inc.php
+		sed -ie "s/define('_PS_MODE_DEV_', false);/define('_PS_MODE_DEV_',\ true);/g" /var/www/$PS_DOMAIN/config/defines.inc.php
 	fi
 
 	if [ $PS_HOST_MODE -ne 0 ]; then
 		echo "\n* Enabling HOST mode ...";
-		echo "define('_PS_HOST_MODE_', true);" >> /var/www/html/config/defines.inc.php
+		echo "define('_PS_HOST_MODE_', true);" >> /var/www/$PS_DOMAIN/config/defines.inc.php
 	fi
 
 	if [ $PS_FOLDER_INSTALL != "install" ]; then
 		echo "\n* Renaming install folder as $PS_FOLDER_INSTALL ...";
-		mv /var/www/html/install /var/www/html/$PS_FOLDER_INSTALL/
+		mv /var/www/html/install /var/www/$PS_DOMAIN/$PS_FOLDER_INSTALL/
 	fi
 
 	if [ $PS_FOLDER_ADMIN != "admin" ]; then
 		echo "\n* Renaming admin folder as $PS_FOLDER_ADMIN ...";
-		mv /var/www/html/admin /var/www/html/$PS_FOLDER_ADMIN/
+		mv /var/www/html/admin /var/www/$PS_DOMAIN/$PS_FOLDER_ADMIN/
 	fi
 
 	if [ $PS_HANDLE_DYNAMIC_DOMAIN = 0 ]; then
@@ -59,19 +59,19 @@ if [ ! -f ./config/settings.inc.php  ]; then
 			mysqladmin -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD create $DB_NAME --force 2> /dev/null;
 		fi
 
-		php /var/www/html/$PS_FOLDER_INSTALL/index_cli.php --domain=$(hostname -i) --db_server=$DB_SERVER:$DB_PORT --db_name="$DB_NAME" --db_user=$DB_USER \
+		php /var/www/$PS_DOMAIN/$PS_FOLDER_INSTALL/index_cli.php --domain=$(hostname -i) --db_server=$DB_SERVER:$DB_PORT --db_name="$DB_NAME" --db_user=$DB_USER \
 			--db_password=$DB_PASSWD --firstname="John" --lastname="Doe" \
 			--password=$ADMIN_PASSWD --email="$ADMIN_MAIL" --language=$PS_LANGUAGE --country=$PS_COUNTRY \
 			--newsletter=0 --send_email=0
 
-		chown www-data:www-data -R /var/www/html/
+		chown nginx:nginx -R /var/www/$PS_DOMAIN/
 	fi
 fi
 
 # We need to remove the pid file or Apache won't start after being stopped
-if [ -f /var/run/apache2/apache2.pid  ]; then
-    rm -f /var/run/apache2/apache2.pid
-fi
+#if [ -f /var/run/apache2/apache2.pid  ]; then
+#    rm -f /var/run/apache2/apache2.pid
+#fi
 
-echo "\n* Almost ! Starting Apache now\n";
-exec apache2 -DFOREGROUND
+#echo "\n* Almost ! Starting Apache now\n";
+#exec apache2 -DFOREGROUND
